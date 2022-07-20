a730cd5e4702bb02a52f749bfd15676da059a0b8
Minh Trung <nguyenducminhtrung0044@gmail.com>
2022-07-20 22:22:07 +0700

    Create All Commponent

---
+++ src/components/footer/index.js
@@ -0,0 +1,10 @@
+import style from './footer.module.scss'
+
+
+function Footer() {
+    return (  
+        <div> chư bươc đi cùng nhau ta hãy cùng đi tới phương trời xa và ta có nhớ chăng khi nào</div>
+    );
+}
+
+export default Footer
